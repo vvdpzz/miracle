85b15448b23e5462532fb5cc2a28ae83a569fcaf
vvdpzz <vvdpzz@gmail.com>
2012-01-28 15:09:49 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miracle::Application.config.secret_token = 'e573ec01f33a3cc01c7bdd9048ccdabf4967f9bdc7b26c4cb02e4ad03264b60ac171fc1609940520440d6e52fe1de4a5f7349d4b9bce305deb1aa1ea9633e65e'

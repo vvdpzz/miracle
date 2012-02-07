095e6fde56d937ca74a899d8ec058ebf1b6f4cfb
vvdpzz <vvdpzz@gmail.com>
2012-02-07 20:09:30 +0800

    silence assets

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,8 @@ module Miracle
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.assets.logger = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
